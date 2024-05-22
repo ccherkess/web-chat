8958a328b41b3d1e3d58101420e42810680a5d32
Yulia Protsenko <prooyuul@gmail.com>
2024-05-23 01:07:56 +0300

    refactor MainController

--- src/main/java/com/etu/chat/controller/MainController.java
+++ src/main/java/com/etu/chat/controller/MainController.java
@@ -8,22 +8,30 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.etu.chat.repository.ChatUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.etu.chat.service.ChatUserService;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
 public class MainController {
     @Autowired
-    private ChatUserRepository chatUserRepository;
+    private ChatUserService chatUserService;
 
     @GetMapping("/")
     public String getMainPage(Model model, @AuthenticationPrincipal UserDetails userDetails) {
+        HashMap<Object, Object> data = new HashMap<>();
 
-        model.addAttribute(
-                "isAdmin",
-                userDetails.getAuthorities().stream()
-                        .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ADMIN"))
-        );
+        data.put("profile", userDetails);
+        if (userDetails != null) {
+            data.put("isAdmin", chatUserService.hasAuthority(userDetails.getUsername(), Authority.builder().name("ROLE_ADMIN").build()));
+        } else {
+            data.put("isAdmin", false);
+        }
+        model.addAttribute("frontendData", data);
+
+        System.out.println("Data sent to frontend: " + data);
+
         return "index";
     }
 
