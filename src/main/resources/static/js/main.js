ab95acb1bde9ed3830bde0bcffa2721f6a07dc18
ccherkess <misha.tcherkasov2015@gmail.com>
2024-05-23 22:45:49 +0300

    setting style

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 import router from 'router/router'
 import App from 'pages/App.vue'
+
+Vue.use(Vuetify)
+
+socket.connect()
 
 new Vue({
     el: '#app',
